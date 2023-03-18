95679b52a915c9128012a828b37b5eefa2e1e714
robertosixty1 <roberto011herminegildo@gmail.com>
2023-03-18 11:30:46 -0300

    Add "clean_all" subcommand to `build.sh`

--- build.sh
+++ build.sh
@@ -47,6 +47,10 @@ build_project() {
 if [ "$1" = "clean" ]; then
     find . -iname "*~" -exec rm {} +
     rm -r bin/
+elif [ "$1" = "clean_all" ]; then
+    find . -iname "*~" -exec rm {} +
+    rm -r bin/
+    git clean -fdx
 else
     build_webcam_rs
     build_image_rs
