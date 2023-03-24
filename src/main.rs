ecb4db0b136813a136aee887c2a16851967ef57d
robertosixty1 <roberto011herminegildo@gmail.com>
2023-03-24 20:49:03 -0300

    Now the widgets can be configured via the `config.toml` file

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,9 @@ use serenity::framework::standard::StandardFramework;
 use serenity::model::gateway::Ready;
 use serenity::prelude::*;
 
-use floatwidgets::widgets::*;
+use serde::Deserialize;
 
-use toml::Table;
+use floatwidgets::widgets::*;
 
 mod commands;
 
@@ -31,45 +31,89 @@ impl EventHandler for Handler {
     }
 }
 
+#[derive(Deserialize)]
+struct DiscordConfig {
+    token: String,
+}
+
+#[derive(Deserialize)]
+struct CameraConfig {
+    enabled: bool,
+    border_color: String,
+}
+
+#[derive(Deserialize)]
+struct ImageConfig {
+    enabled: bool,
+    image_path: String,
+}
+
+#[derive(Deserialize)]
+struct TextConfig {
+    enabled: bool,
+    text: String,
+    background_image: String,
+}
+
+#[derive(Deserialize)]
+struct WidgetConfig {
+    camera: CameraConfig,
+    image: ImageConfig,
+    text: TextConfig,
+}
+
+#[derive(Deserialize)]
+struct Config {
+    discord: DiscordConfig,
+    widgets: WidgetConfig,
+}
+
 #[tokio::main]
 async fn main() {
-    let toml: Table;
+    let config: Config;
     if let Ok(text) = read_to_string("config.toml") {
-        if let Ok(table) = text.parse::<Table>() {
-            toml = table;
-        } else {
-            eprintln!("ERROR: Could not parse file `config.toml`");
-            exit(1);
+        match toml::from_str(text.as_str()) {
+            Ok(table) => {
+                config = table;
+            }
+            Err(e) => {
+                eprintln!("ERROR: Could not parse file `config.toml`\n{}", e);
+                exit(1);
+            }
         }
     } else {
         eprintln!("ERROR: Could read file `config.toml`");
         exit(1);
     }
 
-    spawn(|| {
-        floatimg::floatimg(
-            "/media/roberto/HomePartition/Pictures/Cursed Emojis/the spiffing brit.png".to_string(),
-        );
-    });
+    if config.widgets.image.enabled {
+        spawn(|| {
+            floatimg::floatimg(config.widgets.image.image_path);
+        });
+    }
 
-    spawn(|| {
-        floatwebcam::floatwebcam();
-    });
+    if config.widgets.camera.enabled {
+        spawn(|| {
+            floatwebcam::floatwebcam();
+        });
+    }
 
-    spawn(|| {
-        floatext::floatext(
-            "assets/Cantarell.ttf".to_string(),
-            "Hello, World!".to_string(),
-            String::new(),
-        );
-    });
+    if config.widgets.text.enabled {
+        spawn(|| {
+            floatext::floatext(
+                "assets/Cantarell.ttf".to_string(),
+                config.widgets.text.text,
+                config.widgets.text.background_image,
+            );
+        });
+    }
 
     let framework = StandardFramework::new()
         .configure(|c| c.prefix("!")) // set the bot's prefix to "!"
         .help(&HELP_CMD)
         .group(&COMMANDS_GROUP);
 
-    let token = toml["discord_token"].as_str().unwrap();
+    let token = config.discord.token.as_str();
     let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
     let mut client = Client::builder(token, intents)
         .event_handler(Handler)
